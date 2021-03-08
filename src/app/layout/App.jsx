0eb669d26360a329a633b812cb2dc9143ae81c27
Asif Bashir <iamasifbashir@gmail.com>
2021-03-08 13:09:58 +1100

    Create event functionality completed

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,16 @@
 import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar";
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function App() {
   const [formOpen, setFormOpen] = useState(false);
   return (
     <>
       <NavBar setFormOpen={setFormOpen} />
-      <Container className='main'>
+      <Container className="main">
         <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
       </Container>
-      
     </>
   );
 }
